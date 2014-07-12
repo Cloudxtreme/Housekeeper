be1d3dafe03a2ca1e8ee32be0ae28c87fe316360
Daniel Middleton <me@daniel-middleton.com>
2014-07-12 18:22:45 +0100

    Started work on Install.sh

---
+++ Install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo
+
+# Check dependancies
+echo 'Checking Housekeeper Dependancies...'
+
+## Ensure Ruby is installed
+ruby_version=$(ruby -v)
+
+if [ $? -ne 0 ]; then
+    echo 'No Ruby found. Installing latest stable version...'
+    \curl -sSL https://get.rvm.io | bash -s stable --ruby
+else
+    echo 'Ruby found.'
+fi
+
+## Check Ruby is up-to-date
+echo 'Ruby version check yet to be implemented.'
+
+## Ensure required gems are installed
+required_gems=(colorize net/smtp yaml logger)
+
+for g in ${required_gems[@]}; do
+    gem_test_response=$(gem which $g)
+    if [ -z "$gem_test_response" ]; then
+        echo "$g Gem not found. Installing..."
+        gem install $g
+    else
+        echo "$g Gem found."
+    fi
+done
+
+echo
+exit 0
