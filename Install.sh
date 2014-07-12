a16ea8589c15e0c6e74c1c505e506a86da5f6aa2
Daniel Middleton <me@daniel-middleton.com>
2014-07-12 23:28:41 +0100

    Making Install.sh more robust.

--- Install.sh
+++ Install.sh
@@ -1,39 +1,62 @@
 #!/bin/bash
+
+# Vars
+tested_ruby_version=2.1.1
+required_gems=(colorize net/smtp yaml logger)
+
 echo
 
 # Check dependancies
 echo 'Checking Housekeeper dependancies...'
 echo
 
-## Ensure Ruby is installed
-ruby_version=$(ruby -v)
+## Ensure Ruby is installed and at compatible version
+ruby_version=$(ruby -e 'print RUBY_VERSION')
 
 if [ $? -ne 0 ]; then
     echo 'No Ruby found. Installing latest stable version...'
+
     \curl -sSL https://get.rvm.io | bash -s stable --ruby
-else
-    echo 'Ruby found.'
+    if [ $? -ne 0 ]; then
+        echo 'Could not install Ruby using RVM. Aborting...'
+        exit 1
+    fi
 fi
 
-## Ruby version check to be implemented.
+bad_version=$(awk "BEGIN{ print "$ruby_version"<"$tested_ruby_version" }")
+
+if [ $bad_version == 1 ]; then
+    echo "Ruby $ruby_version is not compatible with Housekeeper. Installing latest stable version..."
+
+    \curl -sSL https://get.rvm.io | bash -s stable --ruby
+    if [ $? -ne 0 ]; then
+        echo 'Could not install Ruby using RVM. Aborting...'
+        exit 1
+    fi
+else
+    echo "Ruby $tested_ruby_version found."
+fi
 
 ## Ensure required gems are installed
-required_gems=(colorize net/smtp yaml logger)
-
 for g in ${required_gems[@]}; do
     gem_test_response=$(gem which $g)
     if [ -z "$gem_test_response" ]; then
         echo "$g Gem not found. Installing..."
+
         gem install $g
+        if [ $? -ne 0 ]; then
+            echo "Could not install $g Gem. Aborting..."
+            exit 1
+        fi
     else
         echo "$g Gem found."
     fi
 done
 
-## Gem version check to be implemented.
+## Gem version check to be implemented
 
 echo
-echo "Installation completed. Please update 'conf/housekeeper.conf' and use 'ruby Housekeeper.rb' to run. It is also advisable to add Housekeeper as a shell alias."
+echo "Dependancies met. Please update 'conf/housekeeper.conf' and use 'ruby Housekeeper.rb' to run. It is also advisable to add Housekeeper as a shell alias."
 
 echo
 exit 0
