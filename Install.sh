83d5903cbe9d084b0fd9173ac11f6d6d0387bbf9
Daniel Middleton <me@daniel-middleton.com>
2014-07-13 15:02:17 +0100

    Fleshing out installer.

--- Install.sh
+++ Install.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-# Vars
+# Config
 tested_ruby_version=2.1.1
 required_gems=(colorize net/smtp yaml logger)
+install_directory='~/Housekeeper'
 
-# Check dependancies
 echo
+
+###############################################################################
+# Check dependancies
+
 echo 'Checking Housekeeper dependancies...'
 echo
 
 ## Ensure Ruby is installed and at compatible version
-ruby_version=$(ruby -e 'print RUBY_VERSION')
+ruby_version=$(ruby -e 'print RUBY_VERSION') > /dev/null 2>&1
 
 if [ $? -ne 0 ]; then
     read -p 'No Ruby found. Can I install the latest stable version using RVM? (yes/no): ' install_ruby_kb_reply
 	
-	case $install_ruby_kb_reply in
+	case ${install_ruby_kb_reply} in
 		'yes')
     		\curl -sSL https://get.rvm.io | bash -s stable --ruby
     		if [ $? -ne 0 ]; then
@@ -36,9 +40,9 @@ if [ $? -ne 0 ]; then
 	esac
 fi
 
-bad_version=$(awk "BEGIN{ print "$ruby_version"<"$tested_ruby_version" }")
+bad_version=$(awk "BEGIN{ print "${ruby_version}"<"${tested_ruby_version}" }")
 
-if [ $bad_version == 1 ]; then
+if [ ${bad_version} == 1 ]; then
     echo "Ruby $ruby_version is not compatible with Housekeeper. Installing latest stable version..."
 
     \curl -sSL https://get.rvm.io | bash -s stable --ruby
@@ -52,11 +56,11 @@ fi
 
 ## Ensure required gems are installed
 for g in ${required_gems[@]}; do
-    gem_test_response=$(gem which $g)
+    gem_test_response=$(gem which ${g}) > /dev/null 2>&1
     if [ -z "$gem_test_response" ]; then
         echo "$g Gem not found. Installing..."
 
-        gem install $g
+        gem install ${g}
         if [ $? -ne 0 ]; then
             echo "Could not install $g Gem. Aborting..."
             exit 1
@@ -66,8 +70,31 @@ for g in ${required_gems[@]}; do
     fi
 done
 
+###############################################################################
+# Install Housekeeper
+
+## Create and enter installation directory
+mkdir $install_directory
+if [ $? -ne 0 ]; then
+    echo "Unable to create '$install_directory' directory. Aborting..."
+    exit 1
+fi
+
+cd $install_directory
+if [ $? -ne 0 ]; then
+    echo "Unable to enter '$install_directory' directory. Aborting..."
+    exit 1
+fi
+
+## Git clone Housekeeper repo
+git clone https://github.com/daniel-middleton/Housekeeper.git
+if [ $? -ne 0 ]; then
+    echo "Unable to Git clone in '$install_directory' directory. Aborting..."
+    exit 1
+fi
+
 echo
-echo "Dependancies met. Please update 'conf/housekeeper.conf' and use 'ruby Housekeeper.rb' to run. It is also advisable to add Housekeeper as a shell alias."
+echo "Installation complete.\nPlease update 'conf/housekeeper.conf' and use 'ruby Housekeeper.rb' to run.\nIt is also advisable to add Housekeeper as a shell alias."
 echo
 
 exit 0
